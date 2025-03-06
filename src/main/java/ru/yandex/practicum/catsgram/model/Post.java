bd14b5ef033f48383cc0620e6541b9bdd4e05a9c
Cetius <heajler@gmail.com>
2025-03-06 18:49:25 +0300

    Добавить контроллеры в проект

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,9 +1,13 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
+@Getter
+@Setter
 @EqualsAndHashCode(of = {"id"})
 public class Post {
     Long id;
